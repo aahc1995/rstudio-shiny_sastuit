14d2a5092df5337a68e810c843207a79293ea4a9
aahc1995 <36556772+aahc1995@users.noreply.github.com>
2020-03-09 07:40:08 -0600

    Update app.R

--- inst/shinyapp/app.R
+++ inst/shinyapp/app.R
@@ -982,7 +982,7 @@ server <- function(input, output,session) {
     else{
       condicion_token <<- TRUE
       # showModal(popupModal(failed = TRUE))
-      #removeModal()
+      removeModal()
     }
   })
   observeEvent(input$sent,{
@@ -996,8 +996,7 @@ server <- function(input, output,session) {
       consumer_key = api_key,
       consumer_secret = api_secret,
       access_token = acc_token,
-     access_secret = acc_secret
-      #,set_renv = FALSE
+     access_secret = acc_secret,set_renv = FALSE
     )
     
     if(is.null(rate_limit(token)[164,3])){
@@ -1160,5 +1159,3 @@ server <- function(input, output,session) {
 # Run the application 
 shinyApp(ui = ui, server = server)
 
-
-               
